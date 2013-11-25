701759289ea1c8ccc6b8f23b82284b21e8e9c4c0
justinholdstock91 <justinholdstock91@gmail.com>
2013-11-25 10:52:42 -0500

    added more maths to file

--- JMath.js
+++ JMath.js
@@ -18,3 +18,19 @@ JMath.saturate = function(value){
 JMath.clamp = function(min, max, value){
 	return Math.min(max, Math.max(min, value));
 };
+
+JMath.quadtricBezier = function(A, B, C, T){
+  //clamp T to 0 and 1
+  T = JMath.saturate(T);
+  
+  var S = 1 - T;
+  var twoS = 2 * S;
+  var twoT = 2 * T;
+  var o = twoS * A + 2 * (S * T) * B + twoT * C;
+  
+  return o;
+};
+
+JMath.randSign = function(){
+  return Math.random() > 0.5 ? 1 : -1;
+};
